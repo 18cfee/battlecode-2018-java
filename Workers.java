18e38d302d31b39decdcae9ea7214aa6e551c7ab
Carl Fee <15cfee@gmail.com>
2018-01-18 17:01:40 -0700

    seemsToWorkWasJustTryingToFixMessLotOfJaseStoughseemstobei

--- Workers.java
+++ Workers.java
@@ -87,12 +87,12 @@ public class Workers extends Group{
         }else if(state == WorkerStates.GatherKarbonite){
             System.out.println("About to gather karbonite");
             if(harvestPoint != null) {
-                System.out.println("There is a karbonite loc");
+                //System.out.println("There is a karbonite loc");
                 gatherKarbonite();
             }
         }
 
-        moveToTarget(hill);
+        //moveToTarget(hill);
         movableIndex = 0;
         index = 0;
     }
@@ -108,12 +108,14 @@ public class Workers extends Group{
         builtRocketIndex = 0;
         unbuiltRocketIndex = 0;
     }
-    void contBuilding(UnitType type){
+    void contBuilding(UnitType type) throws Exception{
         if(type == UnitType.Factory) {
             if (p.unbuiltFactIndex != 0) {
                 for (int i = 0; i < index; i++) {
                     if (gc.canBuild(ids[i], factBlueId)) {
                         gc.build(ids[i], factBlueId);
+                    }else{
+                        moveToTarget(hill);
                     }
                 }
             }
@@ -125,18 +127,24 @@ public class Workers extends Group{
                     //System.out.println("Trying to work on the rocket: " + rocketBlueId);
                     if(gc.canBuild(ids[i], rocketBlueId)){
                         gc.build(ids[i], rocketBlueId);
+                        //System.out.println("Worked on the rocket");
+                    }else{
+                        moveToTarget(hill);
                     }
                 }
             }
         }
     }
 
-    void gatherKarbonite(){
+    void gatherKarbonite() throws Exception{
         for(int i = 0; i < index; i++){
             if(gc.canHarvest(ids[i], gc.unit(ids[i]).location().mapLocation().directionTo(harvestPoint))){
-                System.out.println("Karbonite harvested!");
+                System.out.println("Karbonite harvested! I shouldn't have moved...");
                 System.out.println("Karbonite left at location: " + gc.karboniteAt(harvestPoint));
                 gc.harvest(ids[i], gc.unit(ids[i]).location().mapLocation().directionTo(harvestPoint));
+            }else{
+                System.out.println("Couldn't harvest, so I'm going to move");
+                moveToTarget(hill);
             }
         }
     }
