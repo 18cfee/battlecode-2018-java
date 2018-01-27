1a5b6223ae9318167235345e5ebde2a336a95f49
Jaseton1 <jaserost@yahoo.com>
2018-01-27 05:21:21 -0700

    prioritizes putting down a factory

--- Workforce.java
+++ Workforce.java
@@ -19,6 +19,7 @@ public class Workforce {
     private HashSet<Integer> oldBuilders;
     private Workers loners;
     private ArrayList<Integer> nonReplicatable;
+    int numWantedBbuilders = 4;
 
     public Workforce(GameController gc, Path p) {
         this.gc = gc;
@@ -34,15 +35,16 @@ public class Workforce {
 
     public void conductTurn() throws Exception{
 
-        System.out.println("There are " + numWorkers + " workers");
-        System.out.println("There are " + builders.size() + " builders");
+        //System.out.println("There are " + numWorkers + " workers");
+        //System.out.println("There are " + builders.size() + " builders");
         int groupNum = 0;
         for(Workers group : gatherers){
             groupNum++;
+            /*
             System.out.println("There are " + group.size() + " workers in gathering group " + groupNum);
             for(int id : group.ids){
-                //System.out.println("Worker " + id + " is in gathering group " + groupNum);
-            }
+                System.out.println("Worker " + id + " is in gathering group " + groupNum);
+            }*/
 
         }
         builders.groupIsAlive = true;
@@ -54,7 +56,7 @@ public class Workforce {
             }
         }
 
-        if (!builders.noUnits() && numWorkers < 10) {
+        if (!builders.noUnits() && numWorkers < 10 && p.getNumFactories() != 0) {
             //System.out.println("Trying to replicate");
             for (int id : builders.ids) {
                 builders.replicate(id);
@@ -83,28 +85,29 @@ public class Workforce {
         builders.conductTurn();
         for(Workers group : gatherers){
             if(group.groupIsAlive) {
-                System.out.println("/**********************************************/");
-                System.out.println("Starting \"gatherKarbonite\"");
+                //System.out.println("/**********************************************/");
+                //System.out.println("Starting \"gatherKarbonite\"");
                 gatherKarbonite(group);
-                System.out.println("/**********************************************/");
+                //System.out.println("/**********************************************/");
             }else{
-                System.out.println("group is dead");
+                //System.out.println("group is dead");
             }
         }
         for (Workers group : gatherers) {
             if (group.groupIsAlive) {
-                System.out.println("/**********************************************/");
-                System.out.println("Starting \"conductTurn\"");
+                //System.out.println("/**********************************************/");
+                //System.out.println("Starting \"conductTurn\"");
                 group.conductTurn();
-                System.out.println("/**********************************************/");
+                //System.out.println("/**********************************************/");
             }else{
-                System.out.println("group is uber-dead");
+                //System.out.println("group is uber-dead");
             }
         }
     }
 
     private void gatherKarbonite(Workers group){
         group.setState(WorkerStates.GatherKarbonite);
+        /*
         if(group.harvestPoint != null){
             if(gc.canSenseLocation(group.harvestPoint)) {
                 System.out.println("Karbonite at last location: " + gc.karboniteAt(group.harvestPoint));
@@ -115,15 +118,16 @@ public class Workforce {
         }else{
             System.out.println("The last location is null");
         }
+        */
         if (group.harvestPoint == null || (gc.canSenseLocation(group.harvestPoint) && gc.karboniteAt(group.harvestPoint) == 0)) {
             boolean viable = false;
-            System.out.println("Picking a new location");
+            //System.out.println("Picking a new location");
             while (!viable && !p.closestKarbLocs.isEmpty()) {
-                System.out.println("The pq isn't empty yet");
+                //System.out.println("The pq isn't empty yet");
                 if (gc.canSenseLocation(p.closestKarbLocs.peek().toMapLocation())) {
                     group.harvestPoint = p.closestKarbLocs.pop().toMapLocation();
-                    System.out.println("New harvest loc picked out: " + group.harvestPoint.toString());
-                    System.out.println("PQ says there are " + p.closestKarbLocs.getSize() + " deposits left");
+                    //System.out.println("New harvest loc picked out: " + group.harvestPoint.toString());
+                    //System.out.println("PQ says there are " + p.closestKarbLocs.getSize() + " deposits left");
                     if (gc.karboniteAt(group.harvestPoint) != 0) {
                         viable = true;
                         if(group.karbLocInSight){
@@ -133,7 +137,7 @@ public class Workforce {
                         group.karbLocInSight = true;
                     }
                 } else {
-                    System.out.println("Can't see the next location: " + p.closestKarbLocs.peek().toMapLocation().toString());
+                    //System.out.println("Can't see the next location: " + p.closestKarbLocs.peek().toMapLocation().toString());
                     viable = true;
                     if (group.onWayToOutofSight) {
                         short[][] hill = p.generateHill(p.closestKarbLocs.peek().toMapLocation());
@@ -146,13 +150,13 @@ public class Workforce {
             }
             if (!viable) {
                 group.setState(WorkerStates.Standby);
-                System.out.println("All outta locs");
+                //System.out.println("All outta locs");
                 return;
             }
         } else {
             group.karbLocInSight = false;
             if (p.closestKarbLocs.isEmpty()) {
-                System.out.println("The pq is empty");
+                //System.out.println("The pq is empty");
                 group.setState(WorkerStates.Standby);
                 return;
             }
@@ -180,17 +184,22 @@ public class Workforce {
     private int numWorkerGroups = 0;
     public void addWorker(int id) throws Exception{
         if(workerRound != p.round) {
-            System.out.println("\n\n\nRefreshing");
+            //System.out.println("\n\n\nRefreshing");
             workerRound = p.round;
             oldGatherers.clear();
             nonReplicatable.clear();
             numWorkerGroups = gatherers.size();
             oldBuilders = (HashSet<Integer>) builders.ids.clone();
             builders.ids.clear();
+            if(numWorkers <= 4){
+                numWantedBbuilders = 2;
+            }else{
+                numWantedBbuilders = 4;
+            }
 
-            System.out.println(oldBuilders.size() + " workers in oldBuilders");
+            //System.out.println(oldBuilders.size() + " workers in oldBuilders");
             for(int check : oldBuilders) {
-                System.out.println("Worker " + check + " is in oldBuilders");
+                //System.out.println("Worker " + check + " is in oldBuilders");
                 if(gc.unit(check).abilityHeat() > 0){
                     nonReplicatable.add(check);
                 }
@@ -199,9 +208,10 @@ public class Workforce {
             for (int i = 0; i < numWorkerGroups; i++) {
                 oldGatherers.add((HashSet<Integer>)gatherers.get(i).ids.clone());
                 gatherers.get(i).ids.clear();
+                /*
                 for(int ids : oldGatherers.get(i)){
                     System.out.println("Worker " + ids + " is in oldGatherers group " + i);
-                }
+                }*/
             }
             /*
             gatherers.clear();
@@ -212,7 +222,7 @@ public class Workforce {
         }
         if(p.movesToBase(gc.unit(id).location().mapLocation()) == 0 && gc.unit(id).location().mapLocation() != p.baseLoc){
             loners.add(id);
-            System.out.println("Worker " + id + " added to loners");
+            //System.out.println("Worker " + id + " added to loners");
             return;
         }else {
             numWorkers++;
@@ -228,19 +238,19 @@ public class Workforce {
         }
 
         //System.out.println("Checking for old builders");
-        if(oldBuilders.contains(id) && builders.size() < 4){
-            System.out.println("\tWorker " + id + " added to builders from OldBuilders");
+        if(oldBuilders.contains(id) && builders.size() < numWantedBbuilders){
+            //System.out.println("\tWorker " + id + " added to builders from OldBuilders");
             builders.add(id);
             return;
         }
 
 
         //System.out.println("Checking for old gatherers");
-        if(oldGatherers.size() > 0  && oldBuilders.size() >= 4) {
+        if(oldGatherers.size() > 0  && oldBuilders.size() >= numWantedBbuilders) {
             for (int i = 0; i < oldGatherers.size(); i++) {
                 if (oldGatherers.get(i).contains(id)) {
                     gatherers.get(i).add(id);
-                    System.out.println("\tWorker " + id + " added to gathering group " + i + " from OldGatherers");
+                    //System.out.println("\tWorker " + id + " added to gathering group " + i + " from OldGatherers");
                     return;
                 }
             }
@@ -248,7 +258,7 @@ public class Workforce {
 
         //System.out.println("Checking to see if we need builders");
         if(builders.size() < 4){
-            System.out.println("\tWorker " + id + " added to builders");
+            //System.out.println("\tWorker " + id + " added to builders");
             builders.add(id);
             return;
         }
@@ -262,7 +272,7 @@ public class Workforce {
         for (int i = 0; i < gatherers.size(); i++) {
             if (gatherers.get(i).size() < 2) {
                 gatherers.get(i).add(id);
-                System.out.println("\tWorker " + id + " added to gathering group " + i);
+                //System.out.println("\tWorker " + id + " added to gathering group " + i);
                 return;
             } else if (i == gatherers.size() - 1) {
                 gatherers.add(new Workers(gc, p));
